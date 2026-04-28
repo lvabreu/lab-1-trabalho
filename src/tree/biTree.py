fd00b6ec8f459f2c3ba50fc41efe54ab96de580f
lvabreu <livia.abreumacedo@gmail.com>
2026-04-28 13:46:12 -0300

    Implementação busca

--- src/tree/biTree.py
+++ src/tree/biTree.py
@@ -25,13 +25,13 @@ class cArvoreBinaria:
     self.__raiz   = None #cabeça da árvore (pai)
     self.__numNos = 0 #quantidade de nós, vetor posição árvore
     self.__raiz   = self.__MontaAB(n)
-
+    
 # *******************************************************
   def getNumNos(self):
     return self.__numNos
 
 # *******************************************************
-  def percorreArvore(self, percurso=percursos.PRE_ORDEM):
+  def percorreArvore(self, percurso=percursos.POST_ORDEM):
 
     if percurso == percursos.PRE_ORDEM:
       self.__preOrdem(self.__raiz)
@@ -82,6 +82,18 @@ class cArvoreBinaria:
       print(raiz.getDado())
 
 # *******************************************************
+  def buscaDado(self, arvore, dado):
+    raiz = self.__raiz
+    while raiz != None and dado != raiz.getDado(): 
+      if dado < raiz.getDado(): 
+        raiz = raiz.getFilhoEsq()
+      else: 
+        raiz = raiz.getFilhoDir()
+    
+    return raiz is not None
+      
+
+# *******************************************************
 # ***                                                 ***
 # *******************************************************
 if __name__ == '__main__':
@@ -89,18 +101,21 @@ if __name__ == '__main__':
   arvore = cArvoreBinaria(20)
 
   print("chave = ", arvore.chave)
+  print("Busca em árvore binária")
+  encontrou = arvore.buscaDado(7)
+  print(f"Valor encontrado?", encontrou)
 
-  print("Percurso em Pré-Ordem:")
-  print("======================")
-  arvore.percorreArvore(percursos.PRE_ORDEM)
-  print("======================")
+#   print("Percurso em Pré-Ordem:")
+#   print("======================")
+#   arvore.percorreArvore(percursos.PRE_ORDEM)
+#   print("======================")
 
-  print("Percurso em In-Ordem:")
-  print("======================")
-  arvore.percorreArvore(percursos.IN_ORDEM)
-  print("======================")
+#   print("Percurso em In-Ordem:")
+#   print("======================")
+#   arvore.percorreArvore(percursos.IN_ORDEM)
+#   print("======================")
 
-  print("Percurso em Post-Ordem:")
-  print("======================")
-  arvore.percorreArvore(percursos.POST_ORDEM)
-  print("======================")
+#   print("Percurso em Post-Ordem:")
+#   print("======================")
+#   arvore.percorreArvore(percursos.POST_ORDEM)
+#   print("======================")
